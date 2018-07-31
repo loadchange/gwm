15d9428a0bf3be176fef99a7fe0bab41df41d83e
LoadChange <soocto@gmail.com>
2018-07-31 19:30:20 +0800

    fix: Smoothing differences

--- dist/gwm.js
+++ dist/gwm.js
@@ -31,10 +31,6 @@ var Watermark = function Watermark(_ref) {
         x = _ref$x === undefined ? 0 : _ref$x,
         _ref$y = _ref.y,
         y = _ref$y === undefined ? 50 : _ref$y,
-        _ref$xSpace = _ref.xSpace,
-        xSpace = _ref$xSpace === undefined ? 0 : _ref$xSpace,
-        _ref$ySpace = _ref.ySpace,
-        ySpace = _ref$ySpace === undefined ? 0 : _ref$ySpace,
         _ref$font = _ref.font,
         font = _ref$font === undefined ? 'microsoft yahe' : _ref$font,
         _ref$color = _ref.color,
@@ -42,26 +38,24 @@ var Watermark = function Watermark(_ref) {
         _ref$fontSize = _ref.fontSize,
         fontSize = _ref$fontSize === undefined ? 12 : _ref$fontSize,
         _ref$alpha = _ref.alpha,
-        alpha = _ref$alpha === undefined ? 0.5 : _ref$alpha,
+        alpha = _ref$alpha === undefined ? 1 : _ref$alpha,
         _ref$width = _ref.width,
         width = _ref$width === undefined ? 158 : _ref$width,
         _ref$height = _ref.height,
         height = _ref$height === undefined ? 100 : _ref$height,
         _ref$angle = _ref.angle,
-        angle = _ref$angle === undefined ? -6 : _ref$angle;
+        angle = _ref$angle === undefined ? -15 : _ref$angle;
     classCallCheck(this, Watermark);
 
-    this.txt = txt;
-    this.x = x;
-    this.y = y;
-    this.xSpace = xSpace; // x轴间隔
-    this.ySpace = ySpace; // y轴间隔
-    this.font = font;
-    this.color = color;
-    this.fontSize = fontSize; // 字体大小
-    this.alpha = alpha; // 透明度
+    this.txt = txt || '内部资料 请勿外传';
     this.width = width;
     this.height = height;
+    this.x = x;
+    this.y = y;
+    this.font = font;
+    this.fontSize = fontSize;
+    this.color = color;
+    this.alpha = alpha;
     this.angle = angle;
 };
 
@@ -71,13 +65,11 @@ var CanvasWay = function () {
 
         this.watermark = watermark;
         var width = watermark.width,
-            height = watermark.height,
-            xSpace = watermark.xSpace,
-            ySpace = watermark.ySpace;
+            height = watermark.height;
 
         this.canvas = document.createElement('canvas');
-        this.canvas.setAttribute('width', width + xSpace);
-        this.canvas.setAttribute('height', height + ySpace);
+        this.canvas.setAttribute('width', width);
+        this.canvas.setAttribute('height', height);
     }
 
     createClass(CanvasWay, [{
@@ -89,8 +81,6 @@ var CanvasWay = function () {
                 y = _watermark.y,
                 width = _watermark.width,
                 height = _watermark.height,
-                xSpace = _watermark.xSpace,
-                ySpace = _watermark.ySpace,
                 font = _watermark.font,
                 color = _watermark.color,
                 fontSize = _watermark.fontSize,
@@ -98,14 +88,16 @@ var CanvasWay = function () {
                 angle = _watermark.angle;
 
             var ctx = this.canvas.getContext('2d');
+            ctx.clearRect(0, 0, width, height);
             ctx.textBaseline = 'top';
             ctx.textAlign = 'left';
             ctx.fillStyle = color;
             ctx.globalAlpha = alpha;
             ctx.font = fontSize + 'px ' + font;
+            ctx.translate(x, y);
             ctx.rotate(Math.PI / 180 * angle);
-            ctx.translate(-15, ySpace / 2 + y / 2);
-            ctx.fillText(txt, xSpace / 2 + x, ySpace / 2 + y);
+            ctx.translate(-x, -y - fontSize);
+            ctx.fillText(txt, x, y + fontSize);
             return this.canvas.toDataURL();
         }
     }]);
@@ -142,17 +134,15 @@ var ElementWay = function () {
                 alpha = _watermark.alpha,
                 angle = _watermark.angle,
                 width = _watermark.width,
-                height = _watermark.height,
-                xSpace = _watermark.xSpace,
-                ySpace = _watermark.ySpace;
+                height = _watermark.height;
 
             var item = document.createElement('div');
             bindCSS(item, {
                 position: 'relative',
                 width: width, height: height,
                 flex: '0 0 ' + width + 'px',
-                margin: xSpace / 2 + 'px ' + ySpace / 2 + 'px',
-                overflow: 'hidden'
+                overflow: 'hidden',
+                pointerEvents: 'none'
             });
             var span = document.createElement('span');
             span.innerHTML = txt;
@@ -163,8 +153,11 @@ var ElementWay = function () {
                 fontFamily: font,
                 fontSize: fontSize + 'px',
                 color: color,
+                lineHeight: 1.5,
                 opacity: alpha,
+                fontWeight: 400,
                 transform: 'rotate(' + angle + 'deg)',
+                transformOrigin: '0 0',
                 userSelect: 'none',
                 whiteSpace: 'nowrap',
                 overflow: 'hidden'
@@ -177,22 +170,20 @@ var ElementWay = function () {
         value: function render() {
             var _watermark2 = this.watermark,
                 width = _watermark2.width,
-                height = _watermark2.height,
-                xSpace = _watermark2.xSpace,
-                ySpace = _watermark2.ySpace;
+                height = _watermark2.height;
 
             var _ref = document.documentElement || document.body,
                 clientWidth = _ref.clientWidth,
                 clientHeight = _ref.clientHeight;
 
-            var column = Math.ceil(clientWidth / (width + xSpace));
-            var rows = Math.ceil(clientHeight / (height + ySpace));
+            var column = Math.ceil(clientWidth / width);
+            var rows = Math.ceil(clientHeight / height);
             var wrap = document.createElement('div');
             bindCSS(wrap, {
                 display: 'flex',
                 flexWrap: 'wrap',
-                width: (width + xSpace) * column + 'px',
-                height: (height + ySpace) * rows + 'px'
+                width: width * column + 'px',
+                height: height * rows + 'px'
             });
             for (var i = 0; i < column * rows; i++) {
                 wrap.appendChild(this._createItem());
@@ -220,51 +211,62 @@ var SvgWay = function () {
                 width = _watermark.width,
                 height = _watermark.height,
                 color = _watermark.color,
-                xSpace = _watermark.xSpace,
-                ySpace = _watermark.ySpace,
                 font = _watermark.font,
                 fontSize = _watermark.fontSize,
                 alpha = _watermark.alpha,
                 angle = _watermark.angle;
 
-            var svgStr = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"" + (width + xSpace) + "px\" height=\"" + (height + ySpace) + "px\">\n                <text x=\"" + x + "px\" y=\"" + y + "px\" dy=\"12px\"\n                    text-anchor=\"start\"\n                    stroke=\"" + color + "\"\n                    stroke-width=\"1\"\n                    stroke-opacity=\"" + alpha + "\"\n                    fill=\"none\"\n                    transform=\"rotate(" + angle + ", " + x + " " + y + ")\"\n                    font-weight=\"100\"\n                    font-size=\"" + fontSize + "\"\n                    font-family=\"" + font + "\"\n                    >\n                    " + txt + "\n                </text>\n            </svg>";
+            var svgStr = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"" + width + "px\" height=\"" + height + "px\">\n                <text x=\"" + x + "px\" y=\"" + y + "px\" dy=\"" + fontSize + "px\"\n                    text-anchor=\"start\"\n                    stroke=\"" + color + "\"\n                    stroke-opacity=\"" + alpha + "\"\n                    fill=\"none\"\n                    transform=\"rotate(" + angle + "," + x + " " + y + ")\"\n                    font-weight=\"100\"\n                    font-size=\"" + fontSize + "\"\n                    font-family=\"" + font + "\"\n                    >\n                    " + txt + "\n                </text>\n            </svg>";
             return "data:image/svg+xml;base64," + window.btoa(unescape(encodeURIComponent(svgStr)));
         }
     }]);
     return SvgWay;
 }();
 
-var gwmDiv = document.createElement('div');
-var gwmStyle = {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    width: '158px',
-    height: '100px',
-    overflow: 'hidden',
-    // backgroundImage: `url("${img}")`,
-    backgroundRepeat: 'no-repeat'
-};
-for (var key in gwmStyle) {
-    gwmDiv.style[key] = gwmStyle[key];
+function getGwm() {
+    var gwmDiv = document.createElement('div');
+    var gwmStyle = {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        width: '178px',
+        height: '120px',
+        overflow: 'hidden',
+        backgroundRepeat: 'no-repeat'
+    };
+    for (var key in gwmStyle) {
+        gwmDiv.style[key] = gwmStyle[key];
+    }
+    return gwmDiv;
 }
 
-var w = new Watermark({ txt: '20180727 内部资料 请勿外传', angle: -15, color: '#ff0000' });
+var w = new Watermark({
+    width: 178,
+    height: 120,
+    txt: '20180727 内部资料 请勿外传', angle: -15,
+    x: 10, y: 60,
+    color: '#ff0000'
+});
 //
 var img = new CanvasWay(w).render();
-gwmDiv.style.backgroundImage = 'url("' + img + '")';
-document.body.appendChild(gwmDiv);
-//
-// w.color='#2196f3'
-// const img = new SvgWay(w).render()
-// gwmDiv.style.backgroundImage = `url("${img}")`
-// document.body.appendChild(gwmDiv)
+var gwm1 = getGwm();
+gwm1.style.backgroundImage = 'url("' + img + '")';
+document.body.appendChild(gwm1);
 
+// success
+w.color = '#2196f3';
+w.txt = '20180727 内部资料 请勿';
+var img2 = new SvgWay(w).render();
+var gwm2 = getGwm();
+gwm2.style.backgroundImage = 'url("' + img2 + '")';
+document.body.appendChild(gwm2);
 
-// w.color = '#000'
-// const elementWay = new ElementWay(w)
-// gwmDiv.appendChild(elementWay.render())
-// document.body.appendChild(gwmDiv)
+w.color = '#000';
+w.txt = '20180727 内部资料';
+var elementWay = new ElementWay(w);
+var gwm3 = getGwm();
+gwm3.appendChild(elementWay.render());
+document.body.appendChild(gwm3);
 //# sourceMappingURL=gwm.js.map
