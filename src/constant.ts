b02086b4637eef18c7eede2adcc248cc88651275
LoadChange <soocto@gmail.com>
2020-02-18 10:20:54 +0800

    feat: src/constant.ts

---
+++ src/constant.ts
@@ -0,0 +1,12 @@
+export const DEFAULT_STYLE = Object.create({
+  position: 'fixed',
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+  overflow: 'hidden',
+  zIndex: -10,
+  backgroundRepeat: 'no-repeat',
+  display: 'block',
+  opacity: '1',
+}) as CSSStyleDeclaration;
