c81d1d65881d83a8ebe20d2ee6f574a776898f6d
LoadChange <soocto@gmail.com>
2020-02-20 17:01:12 +0800

    feat: src/core/element.ts

---
+++ src/core/element.ts
@@ -0,0 +1,64 @@
+import Watermark from '../watermark';
+import bindCSS from '../helpers/bindCSS';
+
+class ElementWay {
+  private readonly watermark: Watermark;
+
+  constructor(watermark: Watermark) {
+    this.watermark = watermark;
+  }
+
+  private createItem(): HTMLDivElement {
+    const { txt, x, y, font, color, fontSize, alpha, angle, width, height } = this.watermark;
+    const item: HTMLDivElement = document.createElement('div');
+    bindCSS(item, Object.create({
+      position: 'relative',
+      width: width + 'px',
+      height: height + 'px',
+      flex: `0 0 ${ width }px`,
+      overflow: 'hidden',
+      pointerEvents: 'none',
+    }) as CSSStyleDeclaration, 'normal');
+    const span = document.createElement('span') as HTMLSpanElement;
+    span.innerHTML = txt;
+    bindCSS(span, Object.create({
+      position: 'absolute',
+      top: `${ y }px`,
+      left: `${ x }px`,
+      fontFamily: font,
+      fontSize: `${ fontSize }px`,
+      color,
+      lineHeight: 1.5,
+      opacity: alpha,
+      fontWeight: 400,
+      transform: `rotate(${ angle }deg)`,
+      transformOrigin: '0 0',
+      userSelect: 'none',
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+    }) as CSSStyleDeclaration, 'normal');
+    item.appendChild(span);
+    return item;
+  }
+
+  public render(): HTMLDivElement {
+    let i = 0;
+    const { width, height } = this.watermark;
+    const { clientWidth, clientHeight } = document.documentElement || document.body;
+    const column = Math.ceil(clientWidth / width);
+    const rows = Math.ceil(clientHeight / height);
+    const wrap = document.createElement('div') as HTMLDivElement;
+    bindCSS(wrap, Object.create({
+      display: 'flex',
+      flexWrap: 'wrap',
+      width: `${ width * column }px`,
+      height: `${ height * rows }px`,
+    }) as CSSStyleDeclaration, 'normal');
+    for (; i < column * rows; i++) {
+      wrap.appendChild(this.createItem());
+    }
+    return wrap;
+  }
+}
+
+export default ElementWay;
