dcc1b7d41c5ec70bfe86cacfdd40649633ebf206
LoadChange <soocto@gmail.com>
2020-02-24 18:37:12 +0800

    feat: src/helpers/bindCSS.ts

---
+++ src/helpers/bindCSS.ts
@@ -0,0 +1,26 @@
+import { humpToLine } from './util';
+
+export const isSupport = (attribute: string): boolean => attribute in document.documentElement.style;
+
+export const assignStyle = (...args: Array<CSSStyleDeclaration | undefined>): CSSStyleDeclaration => {
+  const [ oldStyle, ...newStyles ] = (args.filter(item => item) as CSSStyleDeclaration[]);
+  return Object.assign.apply(null, [ oldStyle, ...newStyles ]) as CSSStyleDeclaration;
+};
+
+export default (elem: HTMLElement, css: CSSStyleDeclaration, priority?: string | null): HTMLElement => {
+  const mergeStyle = assignStyle(elem.style, css);
+  for (const key in mergeStyle) {
+    if (priority === 'normal') {
+      elem.style[key] = mergeStyle[key];
+      continue;
+    }
+    if (elem.style.setProperty) {
+      elem.style.setProperty(key, css[key], 'important');
+      continue;
+    }
+    const oldStyle = elem.getAttribute('style');
+    const newStyle = [ humpToLine(key), `${ css[key] }!important;` ].join(':');
+    elem.setAttribute('style', [ oldStyle, newStyle ].join(' ').trim());
+  }
+  return elem;
+};
